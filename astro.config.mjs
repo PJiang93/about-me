f528507ba72802ad8a8305c2af9bb4f81a0b302a
JAaron9311 <JAaron9311@users.noreply.github.com>
2023-04-15 21:31:00 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
 export default defineConfig({
-  site: 'https://pjiang93.github.io',
-  base: '/peng.jiang.github.io'
+  site: 'https://peng.jiang.com'
 })
 // https://astro.build/config
 // export default defineConfig({
