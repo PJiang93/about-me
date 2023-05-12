d0d434c065a2437bed20c9b51298a0d5f51148ee
JAaron9311 <JAaron9311@users.noreply.github.com>
2023-05-12 14:47:15 -0400

    Add google analytics to Astro with Partytown
    https://www.kevinzunigacuellar.com/blog/google-analytics-in-astro/

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://github.com/PJiang93',
   base: '/pjiang.github.io',
-  integrations: [mdx(), sitemap(), tailwind()]
-
-})
+  integrations: [mdx(), sitemap(), tailwind(),     
+    partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),]
+});
 // https://astro.build/config
 // export default defineConfig({
 //   site: 'https://astro-modern-personal-website.netlify.app',
